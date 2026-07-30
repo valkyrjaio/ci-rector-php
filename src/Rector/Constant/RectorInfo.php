4ac31777087f6b0ca868b2e1879b9b4b5701a723
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-07-30 05:43:53 +0000

    [v26.2.0] chore: Update RectorInfo for the release

--- src/Rector/Constant/RectorInfo.php
+++ src/Rector/Constant/RectorInfo.php
@@ -20,12 +20,12 @@ final class RectorInfo
      *
      * @var non-empty-string
      */
-    public const string VERSION = '26.1.11';
+    public const string VERSION = '26.2.0';
 
     /**
      * The Rector package version build datetime.
      *
      * @var non-empty-string
      */
-    public const string VERSION_BUILD_DATE_TIME = 'July 27 2026 08:55:44 MST';
+    public const string VERSION_BUILD_DATE_TIME = 'July 29 2026 22:43:06 MST';
 }
